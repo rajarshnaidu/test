7b6e847fea4efc3ca0c16edc746a6062734024bc
Rajarsh <56415666+rajarshnaidu@users.noreply.github.com>
2024-09-18 12:26:00 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,8 +2,8 @@
 param_file=$1
 
 # Read the values from the JSON file
-COD1_url=$(sed -n 's/.*"COD1_url": "\(.*\)",/\1/p' "$param_file")
-COD2_url=$(sed -n 's/.*"COD2_url": "\(.*\)"/\1/p' "$param_file")
+COD1_url=$(cat $param_file | grep COD1 | awk -F': ' '{print $NF}' | sed -e 's+"++g' -e 's+,++g')
+COD2_url=$(cat $param_file | grep COD2 | awk -F': ' '{print $NF}' | sed -e 's+"++g' -e 's+,++g')
 
-# Swap the values in the JSON file
+#Update the variables
 sed -i.bak -e "s|\"COD1_url\": \"$COD1_url\"|\"COD1_url\": \"$COD2_url\"|" -e "s|\"COD2_url\": \"$COD2_url\"|\"COD2_url\": \"$COD1_url\"|" "$param_file"
